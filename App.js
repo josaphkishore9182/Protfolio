95940ead1da90caf9e8340b5c6d602539d00b0d4
KAKARLAMUDI JOSEPH KISHORE <131544586+josaphkishore9182@users.noreply.github.com>
2024-07-23 21:10:07 +0530

    Main files

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import "./App.css"
+import Mainpage from './shoppingfolder/page/Mainpage'
+
+function App() {
+  return (
+    <div>
+      <Mainpage />
+    </div>
+  )
+}
+
+export default App
